eb1cee3176d2a0d64c3f21e7adc7105e4e056860
caesar003 <caesarmuksid@gmail.com>
2023-01-12 05:44:06 +0700

    chore: cleaning thngs up

--- index.js
+++ index.js
@@ -25,36 +25,26 @@ app.get("/", (req, res) => {
 app.post("/api/signin", (req, res) => {
     const { email, password } = req.body;
 
-    userModel.findOne({ email: email }).exec((err, user) => {
+    userModel.findOne({ email }).exec((err, user) => {
         if (err) return res.json({ error: "Failed to login" }).status(500);
-        // Find if email exists
         if (!user) {
             return res
                 .json({ error: "User with that email does not exists" })
                 .status(404);
-        } else {
-            // Check if hash and password matches
-            const isCorrectPassword = bcrypt.compareSync(password, user.hash);
-            if (!isCorrectPassword) {
-                return res.json({ error: "Invalid credentials" }).status(400);
-            }
-            // Exclude the hash field as the response to the client
-            return res.json({
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                phone: user.phone,
-            });
         }
+        const isCorrectPassword = bcrypt.compareSync(password, user.hash);
+        if (!isCorrectPassword) {
+            return res.json({ error: "Invalid credentials" }).status(400);
+        }
+        return res.json(user);
     });
 });
 
 app.post("/api/register", (req, res) => {
     const { password, name, email, phone } = req.body;
 
-    userModel.findOne({ email, email }, (err, user) => {
+    userModel.findOne({ email }, (err, user) => {
         if (err) return res.json({ error: "Error occured" }).status(500);
-        console.log(user);
         if (user)
             return res.json({ error: "Email already exists!" }).status(400);
 
