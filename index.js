35f56b01044580f61c2263fb0a2a05be8f2732d7
caesar003 <caesarmuksid@gmail.com>
2023-01-13 11:57:13 +0700

    refac: migrate from mongoose mongo to supabase postgresql

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ require("dotenv").config();
 const bcrypt = require("bcrypt");
 const bodyParser = require("body-parser");
 const express = require("express");
-const mongoose = require("mongoose");
+const { createClient } = require("@supabase/supabase-js");
 
 const app = express();
-const mongoUri = process.env.MONGO_URI;
+const postgreUri = process.env.POSTGRE_URI;
+const postgreKey = process.env.POSTGRE_ANON_KEY;
 const port = process.env.PORT || 3002;
 
-const { userModel } = require("./src/models");
-
-mongoose
-    .set({ strictQuery: false })
-    .connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
+const supabase = createClient(postgreUri, postgreKey);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -24,36 +21,76 @@ app.get("/", (req, res) => {
 app.post("/api/signin", (req, res) => {
     const { email, password } = req.body;
 
-    userModel.findOne({ email }).exec((err, user) => {
-        if (err) return res.json({ error: "Failed to login" }).status(500);
-        if (!user) {
-            return res
-                .json({ error: "User with that email does not exists" })
-                .status(404);
-        }
-        const isCorrectPassword = bcrypt.compareSync(password, user.hash);
-        if (!isCorrectPassword) {
-            return res.json({ error: "Invalid credentials" }).status(400);
-        }
-        return res.json(user);
-    });
+    supabase
+        .from("students")
+        .select("*")
+        .eq("email", email)
+        .then(({ data }) => {
+            if (data.length === 0) {
+                return res.status(404).json({
+                    error: "User with that email does not exist",
+                });
+            }
+            const student = data[0];
+            const isCorrectPassword = bcrypt.compareSync(
+                password,
+                student.hash
+            );
+            if (!isCorrectPassword) {
+                return res.status(400).json({ error: "Invalid credentials" });
+            }
+            delete student.hash;
+            return res.json(student);
+        })
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).json({ error: "Error occured" });
+        });
 });
 
 app.post("/api/register", (req, res) => {
     const { password, name, email, phone } = req.body;
+    supabase
+        .from("students")
+        .select("*")
+        .eq("email", email)
+        .then(({ data }) => {
+            if (data.length) {
+                return res.status(400).json({
+                    error: "Email already used!",
+                });
+            }
 
-    userModel.findOne({ email }, (err, user) => {
-        if (err) return res.json({ error: "Error occured" }).status(500);
-        if (user)
-            return res.json({ error: "Email already exists!" }).status(400);
-
-        const hash = bcrypt.hashSync(password, 10);
-
-        userModel.create({ name, email, phone, hash }, (err, user) => {
-            if (err) return res.json({ error: "Error occured" }).status(500);
-            return res.json(user);
+            const hash = bcrypt.hashSync(password, 10);
+            supabase
+                .from("students")
+                .insert({
+                    name,
+                    phone,
+                    email,
+                    hash,
+                })
+                .then((data) => {
+                    console.log(data);
+                    return res.json(data);
+                    /**
+                     {
+                            "error": null,
+                            "data": null,
+                            "count": null,
+                            "status": 201,
+                            "statusText": "Created"
+                        }
+                     */
+                })
+                .catch((err) => {
+                    return res.status(500).json({ error: "Error occured!" });
+                });
+        })
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).json({ error: "Error occured!" });
         });
-    });
 });
 
 app.listen(port, () => console.log(`App is running on port: ${port}`));
