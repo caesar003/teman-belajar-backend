9f54098771efbbbea4a8baf5b10df4c750de69a1
caesar003 <caesarmuksid@gmail.com>
2023-01-14 07:34:44 +0700

    feat: added few more endpoints

--- index.js
+++ index.js
@@ -5,17 +5,29 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3002;
 
-const { signin, register } = require("./src/db");
+const { student, lesson, question } = require("./src/db");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.send("This is working");
+    res.status(403).json({ error: "Access resticted!" });
 });
 
-app.post("/api/signin", (req, res) => signin(req.body, res));
+app.get("/api/lesson-list", (req, res) => lesson.get(res));
 
-app.post("/api/register", (req, res) => register(req.body, res));
+app.get("/api/question/:query", (req, res) =>
+    question.search(req.params.query, res)
+);
+
+app.get("/api/question-by-tag/:tag", (req, res) =>
+    question.getByTag(req.params.tag, res)
+);
+
+app.post("/api/signin", (req, res) => student.signin(req.body, res));
+
+app.post("/api/register", (req, res) => student.register(req.body, res));
+
+app.post("/api/add-question", (req, res) => question.add(req.body, res));
 
 app.listen(port, () => console.log(`App is running on port: ${port}`));
--- src/db.js
+++ src/db.js
@@ -7,77 +7,138 @@ const postgreKey = process.env.POSTGRE_ANON_KEY;
 
 const supabase = createClient(postgreUri, postgreKey);
 
-function signin(credentials, res) {
-    const { email, password } = credentials;
-    supabase
-        .from("students")
-        .select("*")
-        .eq("email", email)
-        .then(({ data }) => {
-            if (data.length === 0) {
-                return res.status(404).json({
-                    error: "User with that email does not exist",
-                });
-            }
-            const student = data[0];
-            const isCorrectPassword = bcrypt.compareSync(
-                password,
-                student.hash
-            );
-            if (!isCorrectPassword) {
-                return res.status(400).json({ error: "Invalid credentials" });
-            }
-            delete student.hash;
-            return res.json(student);
-        })
-        .catch((err) => {
-            console.log(err);
-            return res.status(500).json({ error: "Error occured" });
-        });
+class Lesson {
+    get(res) {
+        supabase
+            .from("lessons")
+            .select("*")
+            .then((data) => {
+                console.log(data);
+                return res.json(data);
+            })
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            });
+    }
+    add() {}
 }
 
-function register(credentials, res) {
-    const { password, name, email, phone } = credentials;
-    supabase
-        .from("students")
-        .select("*")
-        .eq("email", email)
-        .then(({ data }) => {
-            if (data.length) {
-                return res.status(400).json({
-                    error: "Email already used!",
-                });
-            }
+class Student {
+    signin(credentials, res) {
+        const { email, password } = credentials;
+        supabase
+            .from("students")
+            .select("*")
+            .eq("email", email)
+            .then(({ data }) => {
+                if (data.length === 0) {
+                    return res.status(404).json({
+                        error: "User with that email does not exist",
+                    });
+                }
+                const student = data[0];
+                const isCorrectPassword = bcrypt.compareSync(
+                    password,
+                    student.hash
+                );
+                if (!isCorrectPassword) {
+                    return res
+                        .status(400)
+                        .json({ error: "Invalid credentials" });
+                }
+                delete student.hash;
+                return res.json(student);
+            })
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json({ error: "Error occured" });
+            });
+    }
+    register(credentials, res) {
+        const { password, name, email, phone } = credentials;
+        supabase
+            .from("students")
+            .select("*")
+            .eq("email", email)
+            .then(({ data }) => {
+                if (data.length) {
+                    return res.status(400).json({
+                        error: "Email already used!",
+                    });
+                }
 
-            const hash = bcrypt.hashSync(password, 10);
-            supabase
-                .from("students")
-                .insert({
-                    name,
-                    phone,
-                    email,
-                    hash,
-                })
-                .then((data) => {
-                    console.log(data);
-                    return res.json(data);
-                    /**
-                     {
-                            "error": null,
-                            "data": null,
-                            "count": null,
-                            "status": 201,
-                            "statusText": "Created"
-                        }
-                     */
-                })
-                .catch((err) => {
-                    return res.status(500).json({ error: "Error occured!" });
-                });
-        })
-        .catch((err) => {
-            console.log(err);
-            return res.status(500).json({ error: "Error occured!" });
-        });
+                const hash = bcrypt.hashSync(password, 10);
+                supabase
+                    .from("students")
+                    .insert({
+                        name,
+                        phone,
+                        email,
+                        hash,
+                    })
+                    .then((data) => {
+                        console.log(data);
+                        return res.json(data);
+                        /**
+                         {
+                                "error": null,
+                                "data": null,
+                                "count": null,
+                                "status": 201,
+                                "statusText": "Created"
+                            }
+                         */
+                    })
+                    .catch((err) => {
+                        return res
+                            .status(500)
+                            .json({ error: "Error occured!" });
+                    });
+            })
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json({ error: "Error occured!" });
+            });
+    }
+    get() {}
 }
-module.exports = { signin, register };
+class Question {
+    constructor() {}
+    add() {}
+    getLatest() {}
+    getByPopularity() {}
+    getByTag(tag, res) {
+        supabase
+            .from("questions")
+            .select()
+            .ilike("tags", `%${tag}%`)
+            .then((data) => {
+                console.log(data);
+                return res.json(data);
+            })
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json({ error: "Error occured!" });
+            });
+    }
+    search(query, res) {
+        supabase
+            .from("questions")
+            .select("*")
+            .ilike("question", `%${query}%`)
+            .then((data) => {
+                console.log(data);
+                return res.json(data);
+            })
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json({ error: "Error occured!" });
+            });
+    }
+}
+const lesson = new Lesson();
+const student = new Student();
+const question = new Question();
+
+module.exports = { student, lesson, question };
