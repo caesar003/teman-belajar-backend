075b6ff1842d9c77f5097d57211f0a876de6bec7
caesar003 <caesarmuksid@gmail.com>
2023-01-16 13:06:42 +0700

    last commit

--- src/db/tag.js
+++ src/db/tag.js
@@ -49,23 +49,16 @@ class Tag {
     }
 
     deleteTagQuestion(questionId, res) {
-        console.log(questionId);
-        // return;
         supabase
             .from("tag_question")
             .delete()
             .eq("question_id", questionId)
             .select("tag_id")
             .then(({ data }) => {
-                // console.log(data);
                 data.forEach((item) => {
-                    // console.log(item);
                     this.deleteTag(item.tag_id);
                 });
 
-                // console.log("tag question deleted")
-
-                // return res.json(data);
                 return supabase
                     .from("questions")
                     .delete()
@@ -78,32 +71,6 @@ class Tag {
                             .status(500)
                             .json({ error: "Error occured!", err });
                     });
-                // capture those tag ids,
-                // check it once again if they are still exists after deletion;
-
-                // supabase
-                //     .from("tag_question")
-                //     .select("*")
-                //     .eq()
-                //     .then()
-                //     .catch((err) => {
-                //         console.log(err);
-                //     });
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }
-
-    stillExists(tagId) {
-        supabase
-            .from("tag_question")
-            .select("*")
-            .eq({ tag_id: tagId })
-            .then((data) => {
-                if (data) return;
-
-                return this.deleteTag(tagId);
             })
             .catch((err) => {
                 console.log(err);
@@ -111,14 +78,12 @@ class Tag {
     }
 
     deleteTag(tagId) {
-        // console.log(tagId);
         supabase
             .from("tag_question")
             .select("*")
             .single()
             .eq("tag_id", tagId)
             .then((data) => {
-                // console.log(data);
                 if (data.error) {
                     return supabase
                         .from("tags")
@@ -136,16 +101,6 @@ class Tag {
             .catch((err) => {
                 console.log(err);
             });
-        // supabase
-        //     .from("tags")
-        //     .delete()
-        //     .eq("id", tagId)
-        //     .then((data) => {
-        //         console.log(data);
-        //     })
-        //     .catch((err) => {
-        //         console.log(err);
-        //     });
     }
 }
 
