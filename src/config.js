b69564743083674fc2e54b78b25341c48ff886f2
caesar003 <caesarmuksid@gmail.com>
2023-01-19 14:05:24 +0700

    refac: I'm totally lost, so I think it's better to get rid of everything and start over

--- src/config.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const { createClient } = require("@supabase/supabase-js");
-
-const postgreUri = process.env.POSTGRE_URI;
-const postgreKey = process.env.POSTGRE_ANON_KEY;
-
-const supabase = createClient(postgreUri, postgreKey);
-
-module.exports = { supabase };
--- src/db/index.js
+++
@@ -1,7 +0,0 @@
-const { lesson } = require("./lesson");
-const { student } = require("./student");
-const { question } = require("./question");
-const { tag } = require("./tag");
-const { answer } = require("./answer");
-
-module.exports = { lesson, student, question, tag, answer };
